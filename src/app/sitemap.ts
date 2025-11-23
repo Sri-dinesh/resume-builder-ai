7f723879cc7deed782b323fb10be7a3ea0492e9e
Sri-Dinesh <santhisridinesh@gmail.com>
2025-11-23 13:29:33 +0530

    fix: Sitemap update.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,29 +1,15 @@
 import { MetadataRoute } from 'next';
 
-const staticRoutes = [
-  '',
-  'tos',
-];
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = [
+    '',
+    '/tos',
+  ];
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { globby } = await import('globby');
-  const pages = await globby([
-    'src/app/(main)/**/page.tsx',
-    '!src/app/(main)/**/success/page.tsx',
-  ]);
-
-  const routes = pages.map((page) => {
-    const path = page
-      .replace('src/app/(main)', '')
-      .replace('/page.tsx', '')
-      .replace('/index', '');
-    return path;
-  });
-
-  const allRoutes = [...staticRoutes, ...routes];
-
-  return allRoutes.map((route) => ({
-    url: `https://sparkcv.vercel.app/${route}`,
+  return routes.map((route) => ({
+    url: `https://sparkcv.vercel.app${route}`,
     lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: route === '' ? 1 : 0.8,
   }));
 }
