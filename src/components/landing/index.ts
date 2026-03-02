f823850c867f419b995eb1ec6bcd43aac11d6e62
Sri-Dinesh <santhisridinesh@gmail.com>
2026-03-02 09:56:39 +0530

    feat: Color Palette

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -1,17 +1,14 @@
-// Landing Page Components - Re-export
-export { Background } from "./Background";
 export { LandingHero } from "./Hero";
 export { Narrative } from "./Narrative";
 export { BentoGrid } from "./BentoGrid";
 export { LandingNavbar } from "./Navbar";
 export { LandingPricing } from "./Pricing";
+export { LandingCTA } from "./CTA";
 export { LandingContact } from "./Contact";
 export { LandingFooter } from "./Footer";
 export { ScrollToTop } from "./ScrollToTop";
 export { MagneticButton } from "./ui/MagneticButton";
 
-// Types
 export type { MagneticButtonProps, BentoCardProps, Theme } from "./types";
 
-// Themes
 export { themes } from "./themes";
